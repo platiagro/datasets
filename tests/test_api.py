7512b0c217140b3fcf66a3f1fb652824c33f21d4
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-01-24 09:56:06 -0200

    Improves unit tests set up and tear down

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,13 +12,31 @@ class TestApi(unittest.TestCase):
 
     def setUp(self):
         # ensures that the bucket exists and is empty
+        self.empty_bucket()
+        self.make_bucket()
+
+    def tearDown(self):
+        # removes bucket after the tests complete
+        self.empty_bucket()
+        self.remove_bucket()
+
+    def empty_bucket(self):
+        # removes bucket after tests are completed
         try:
             for obj in client.list_objects(bucket, prefix="", recursive=True):
                 client.remove_object(bucket, obj.object_name)
         except:
             pass
+
+    def make_bucket(self):
         try:
             client.make_bucket(bucket)
+        except:
+            pass
+
+    def remove_bucket(self):
+        try:
+            client.remove_bucket(bucket)
         except:
             pass
 
