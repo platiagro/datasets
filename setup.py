07ced4ac2c820c88566798631d4323ec3e05ba45
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-02-24 10:08:08 -0300

    Updates setup.py to use PlatIAgro SDK
    - Also uses setuptools.find_packages
    - Makes Python 3.6 min requirement

--- setup.py
+++ setup.py
@@ -17,24 +17,23 @@ setup(
     description="Manages datasets.",
     license="Apache",
     url="https://github.com/platiagro/datasets",
-    packages=[],
+    packages=find_packages(),
     install_requires=[
         # WSGI server
         "Flask==1.1.1",
         "Flask-Cors==3.0.8",
-        # Access object storage server
-        "minio==5.0.6",
-        # Helps with structured data operations
-        "pandas==0.25.3",
+        # PlatIAgro SDK
+        "platiagro @ git+https://github.com/fberanizo/sdk.git@feature/jupyter-notebook-utils",
     ],
     extras_require=extras,
-    python_requires=">=3.5.0",
+    python_requires=">=3.6.0",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
