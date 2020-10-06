e67f1d22b2b04eed9cf1dd6d962dab8e45ba5e01
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-10-06 09:13:43 -0300

    Feature/coco dataset (#43)
    * Allows to crate sample datasets from raw files
    
    Tries to read_csv, if an exception is raised, then saves the raw file.
    This allows to create samples datasets from zip, jpg, and other non
    CSV files.
    
    * Adds a tiny part of COCO dataset as a sample
    
    Taken from: https://registry.opendata.aws/fast-ai-coco/
    
    * Adds tests for non CSV sample datasets

--- datasets/samples.py
+++ datasets/samples.py
@@ -19,17 +19,23 @@ def init_datasets(config_path: str):
             file = dataset["file"]
             filename = file.split("/")[-1]
 
-            df = pd.read_csv(file, sep=None, engine="python")
-            columns = df.columns.values.tolist()
-            featuretypes = infer_featuretypes(df)
-            metadata = {
-                "columns": columns,
-                "featuretypes": featuretypes,
-                "original-filename": filename,
-            }
+            try:
+                data = pd.read_csv(file, sep=None, engine="python")
+                columns = data.columns.values.tolist()
+                featuretypes = infer_featuretypes(data)
+                metadata = {
+                    "columns": columns,
+                    "featuretypes": featuretypes,
+                    "original-filename": filename,
+                }
+            except Exception:
+                # if read/metadata inference fails, then saves raw file
+                metadata = {"original-filename": filename}
+                data = open(file, "rb")
+
             try:
                 # uses PlatIAgro SDK to save the dataset
                 # marks as read only, so users can't mess with these datasets
-                save_dataset(name, df, metadata=metadata, read_only=True)
+                    save_dataset(name, data, metadata=metadata, read_only=True)
             except PermissionError:
                 pass
