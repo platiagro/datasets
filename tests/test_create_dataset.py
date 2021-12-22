c460d15ee7957b1f9eb1ec44ce70e2cc644b6d96
Domingos <dfvneto@gmail.com>
2021-12-22 11:48:30 -0300

    Changed the way the dataset file is opened

--- tests/test_create_dataset.py
+++ tests/test_create_dataset.py
@@ -238,17 +238,18 @@ class TestCreateDataset(unittest.TestCase):
         Should call platiagro.save_dataset using given file, filename, and metadata (columns, featurestypes, total, original-filename).
         """
         dataset_name = util.PREDICT_FILE
+        with util.PREDICT_FILE_HEADER as file:
 
-        rv = TEST_CLIENT.post(
-            "/datasets",
-            files={
-                "file": (
-                    dataset_name,
-                    util.PREDICT_FILE_HEADER,
-                    "multipart/form-data",
-                )
-            },
-        )
+            rv = TEST_CLIENT.post(
+                "/datasets",
+                files={
+                    "file": (
+                        dataset_name,
+                        file,
+                        "multipart/form-data",
+                    )
+                },
+            )
         result = rv.json()
 
         expected = {
