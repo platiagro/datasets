0546a95ff5a214927f14179f55d9a07c0b224639
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2021-12-30 14:24:14 -0300

    Putting time.sleep between tries

--- datasets/api.py
+++ datasets/api.py
@@ -87,6 +87,7 @@ async def handle_post_datasets(
                 return create_dataset(file)
             except Exception as e:
                 logging.info(e)
+                time.sleep(1)
                 i = i + 1
 
         raise InternalServerError("Not able to handle file upload")
